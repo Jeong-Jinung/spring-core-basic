28228c5273201667fe4d95f5629ea498e7c703df
JeongJinUng <jinung.dev@gmail.com>
2021-07-14 00:32:47 +0900

    관심사의 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     // 리파지토리가 추상화와 구체화 모두에 이존하고 있다. DIP 위반
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
