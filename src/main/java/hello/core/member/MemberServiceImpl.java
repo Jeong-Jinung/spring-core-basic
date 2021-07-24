f0ba200aa33a900f27553d530b4f5ea81b90b08e
JeongJinUng <jinung.dev@gmail.com>
2021-07-25 01:53:22 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     // 리파지토리가 추상화와 구체화 모두에 이존하고 있다. DIP 위반
     private final MemberRepository memberRepository;
 
+    @Autowired //ac.getBean(MemberRepository.class) 비슷하게 자동으로 들어가게됨
     public MemberServiceImpl(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
