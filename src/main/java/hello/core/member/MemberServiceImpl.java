818b236832f8342a27290a6b95bcbe71ccc76a43
JeongJinUng <jinung.dev@gmail.com>
2021-07-25 14:26:01 +0900

    컴포넌트스캔 필터

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,6 @@ package hello.core.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 public class MemberServiceImpl implements MemberService {
