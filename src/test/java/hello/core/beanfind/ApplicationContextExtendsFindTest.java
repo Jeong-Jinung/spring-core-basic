5f2cb31efd12e2ac814cf5078a925cdb4238a095
JeongJinUng <jinung.dev@gmail.com>
2021-07-27 23:49:56 +0900

    자동, 수동 주입

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -23,7 +23,7 @@ public class ApplicationContextExtendsFindTest {
     @Test
     @DisplayName("부모 타임으로 조회, 자식이 둘 이상 있으면, 중복 오류가 발생한다")
     void findBeanByParentTypeDuplicate() {
-        DiscountPolicy bean = ac.getBean(DiscountPolicy.class);
+//        DiscountPolicy bean = ac.getBean(DiscountPolicy.class);
         assertThrows(NoUniqueBeanDefinitionException.class,
                 () -> ac.getBean(DiscountPolicy.class));
     }
